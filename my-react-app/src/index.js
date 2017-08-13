9829f20bebb2ad45aea2ae8da742b2009919972a
yashwin <yashwinpoojary@gmail.com>
2017-08-13 13:19:39 +0530

    components and states

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Clock extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    );
+  }
+
+}
+
+function tick() {
+  ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
+
 registerServiceWorker();
