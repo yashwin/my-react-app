2f53991fb40b00b1ce8e33a20c734b127c31aff2
yashwin <yashwinpoojary@gmail.com>
2017-08-22 19:41:20 +0530

    inline if

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -2,74 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-class LoginControl extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-    this.state = {isLoggedIn: false};
-  }
-
-  handleLoginClick() {
-    this.setState({isLoggedIn: true});
-  }
-
-  handleLogoutClick() {
-    this.setState({isLoggedIn: false});
-  }
-
-  render() {
-    const isLoggedIn = this.state.isLoggedIn;
-    let button = null;
-    if(isLoggedIn) {
-      button = <LogoutButton onClick={this.handleLogoutClick} />;
-    }
-    else {
-      button = <LoginButton onClick={this.handleLoginClick} />;
-    }
-    return (
-      <div>
-        <Greeting isLoggedIn={isLoggedIn} />
-        {button}
-      </div>
-    )
-  }
-}
-
-function UserGreeting(props) {
-  return <h1>Welcome back!</h1>;
-}
-
-function GuestGreeting(props) {
-  return <h1>Please sign up.</h1>;
-}
-
-function Greeting(props) {
-  const isLoggedIn = props.isLoggedIn;
-  if (isLoggedIn) {
-    return <UserGreeting />;
-  }
-  return <GuestGreeting />;
-}
-
-function LoginButton(props) {
+function Mailbox(props) {
+  const unreadMessages = props.unreadMessages;
   return (
-    <button onClick={props.onClick}>
-      Login
-    </button>
+    <div>
+      <h1>Hello!!</h1>
+      {unreadMessages.length > 0 &&
+        <div>
+          <h2>
+            You have {unreadMessages.length} unread messages.
+          </h2>
+          <ul>
+            <li>
+              {unreadMessages}
+            </li>
+          </ul>
+        </div>
+      }
+    </div>
   );
 }
 
-function LogoutButton(props) {
-  return (
-    <button onClick={props.onClick}>
-      Logout
-    </button>
-  );
-}
+const messages = ['React', 'Re:React', 'Re:Re:React'];
 
 ReactDOM.render(
-  <LoginControl />,
+  <Mailbox unreadMessages={messages} />,
   document.getElementById('root')
-)
+);
