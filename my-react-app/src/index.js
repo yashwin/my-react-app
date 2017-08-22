e59f3c95950c3108d7540a8c231ecdf2c9d47dc2
yashwin <yashwinpoojary@gmail.com>
2017-08-22 18:33:42 +0530

    handling events

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -2,42 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Clock extends React.Component {
+class Toggle extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {date: new Date()};
+    this.state = {isToggleOn: true};
   }
 
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
   }
 
   render() {
     return (
-      <div>
-        <h1>Hello, world!!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
-      </div>
+      <button onClick={(e) => this.handleClick(e)}>
+        {this.state.isToggleOn ? 'ON': 'OFF'}
+      </button>
     );
   }
-
 }
 
 ReactDOM.render(
-  <Clock />,
+  <Toggle />,
   document.getElementById('root')
 );
